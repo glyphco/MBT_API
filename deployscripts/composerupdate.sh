9f5613560ac8273be3d4e0331bd947075b53fff5
jpittelkow <jpittelkow@gmail.com>
2017-01-05 19:18:51 -0600

    artisan migrate added to api deploy script

--- deployscripts/composerupdate.sh
+++ deployscripts/composerupdate.sh
@@ -20,6 +20,7 @@ echo '---Self-Update Composer Complete---' >> $logfile 2>&1
 #---- 2/2 - Start Composer update----#
 cd $root >> $logfile 2>&1
 sudo composer update >> $logfile 2>&1
+php artisan migrate >> $logfile 2>&1
 echo '---Composer Update Complete---' >> $logfile 2>&1
 
 echo '********************'$(date -u)' - COMPLETE COMPOSER UPDATE Process ********************' >> $logfile 2>&1
