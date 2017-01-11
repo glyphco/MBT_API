ef250bce3b463a76e109a7a07a17d04b03ffccbb
Shawn Dalton <sdalton@aroptions.com>
2017-01-11 01:13:03 -0600

    setup for new participants

--- database/factories/ParticipantFactory.php
+++ database/factories/ParticipantFactory.php
@@ -6,6 +6,15 @@ $factory->define('App\Models\Participant', function (Faker\Generator $faker) {
 	$lat = $faker->latitude($min = -90, $max = 90);
 	$lng = $faker->longitude($min = -180, $max = 180);
 
+//impliment usernames from actual profiles, but dont use the IDs sometimes
+	//$profile_id  = $faker->optional()->randomElement([1, 3, 5, 7, 9]);
+	//$name = $faker->catchPhrase;
+	//$info = $faker->text($maxNbChars = 100);
+	//if ($profile_id) {
+	//$name = Profile::find($profile_id)->name;
+	//$info = Profile::find($profile_id)->info;
+	//}
+
 	return [
 		'name'         => $faker->catchPhrase,
 		'info'         => $faker->text($maxNbChars = 100),
