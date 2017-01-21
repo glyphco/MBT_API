84effd757966a739a998a5879a68ba06c2738390
Shawn Dalton <sdalton@aroptions.com>
2017-01-21 01:33:28 -0600

    added images to events, venue, and profile with seeding

--- database/migrations/2016_12_20_100000_create_venues_table.php
+++ database/migrations/2016_12_20_100000_create_venues_table.php
@@ -4,54 +4,59 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVenuesTable extends Migration {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up() {
-		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-		Schema::dropIfExists('venues');
-		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-		Schema::create('venues', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('name');
-			$table->string('slug', 60)->nullable();
-			$table->string('category');
-			$table->string('street_address');
-			$table->string('city');
-			$table->string('state');
-			$table->string('zipcode');
+class CreateVenuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('venues');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        Schema::create('venues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug', 60)->nullable();
+            $table->string('category');
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
 
-			$table->decimal('lat', 10, 8);
-			$table->decimal('lng', 11, 8);
+            $table->decimal('lat', 10, 8);
+            $table->decimal('lng', 11, 8);
 
-			$table->string('phone');
-			$table->string('email');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('imageurl');
+            $table->string('backgroundurl');
 
-			$table->boolean('public')->default(0);
-			$table->boolean('confirmed')->default(0);
+            $table->boolean('public')->default(0);
+            $table->boolean('confirmed')->default(0);
 
-			$table->timestamps();
-			$table->unsignedInteger('created_by')->nullable()->default(null);
-			$table->unsignedInteger('updated_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->unsignedInteger('created_by')->nullable()->default(null);
+            $table->unsignedInteger('updated_by')->nullable()->default(null);
 
-		});
+        });
 
-		/*Spatial Column*/
-		DB::statement('ALTER TABLE venues ADD location POINT');
+        /*Spatial Column*/
+        DB::statement('ALTER TABLE venues ADD location POINT');
 
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down() {
-		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-		Schema::dropIfExists('venues');
-		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('venues');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
 }
--- database/migrations/2016_12_25_100000_create_profiles_table.php
+++ database/migrations/2016_12_25_100000_create_profiles_table.php
@@ -4,53 +4,59 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfilesTable extends Migration {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up() {
-		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-		Schema::dropIfExists('profiles');
-		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-		Schema::create('profiles', function (Blueprint $table) {
-			$table->increments('id');
-			$table->string('name', 60);
-			$table->string('slug', 60)->nullable()->default(null);
-			$table->string('category')->nullable()->default(null);
-			$table->string('street_address')->nullable()->default(null);
-			$table->string('city')->nullable()->default(null);
-			$table->string('state')->nullable()->default(null);
-			$table->string('zipcode')->nullable()->default(null);
-			$table->string('lat')->nullable()->default(null);
-			$table->string('lng')->nullable()->default(null);
-			$table->string('phone')->nullable()->default(null);
-			$table->string('email')->nullable()->default(null);
-			$table->boolean('participant')->default(0);
-			$table->boolean('production')->default(0);
-			$table->boolean('canhavemembers')->default(0);
-			$table->boolean('canbeamember')->default(0);
-			$table->boolean('public')->default(0);
-			$table->boolean('confirmed')->default(0);
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('profiles');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 60);
+            $table->string('slug', 60)->nullable()->default(null);
+            $table->string('category')->nullable()->default(null);
+            $table->string('street_address')->nullable()->default(null);
+            $table->string('city')->nullable()->default(null);
+            $table->string('state')->nullable()->default(null);
+            $table->string('zipcode')->nullable()->default(null);
+            $table->string('lat')->nullable()->default(null);
+            $table->string('lng')->nullable()->default(null);
+            $table->string('phone')->nullable()->default(null);
+            $table->string('email')->nullable()->default(null);
+            $table->boolean('participant')->default(0);
+            $table->boolean('production')->default(0);
+            $table->boolean('canhavemembers')->default(0);
+            $table->boolean('canbeamember')->default(0);
+            $table->boolean('public')->default(0);
+            $table->boolean('confirmed')->default(0);
 
-			$table->timestamps();
-			$table->unsignedInteger('created_by')->nullable()->default(null);
-			$table->unsignedInteger('updated_by')->nullable()->default(null);
+            $table->string('imageurl');
+            $table->string('backgroundurl');
 
-		});
-		/*Spatial Column*/
-		DB::statement('ALTER TABLE profiles ADD location POINT');
-	}
+            $table->timestamps();
+            $table->unsignedInteger('created_by')->nullable()->default(null);
+            $table->unsignedInteger('updated_by')->nullable()->default(null);
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down() {
-		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-		Schema::dropIfExists('profiles');
-		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-	}
+        });
+        /*Spatial Column*/
+        DB::statement('ALTER TABLE profiles ADD location POINT');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('profiles');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
 }
